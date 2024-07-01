fa648d826862c7bf87ba5bc49150d07732388200
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-07-01 15:02:11 +0300

    template file:Added javascript for ascii art output functionalty

--- main.go
+++ main.go
@@ -3,20 +3,21 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"path/filepath"
 
-	web "web/ascii" // Ensure this matches your module path
+	web "web/ascii"
 )
 
 var templates = template.Must(template.ParseFiles(filepath.Join("templates", "index.html")))
 
 func main() {
+	// fmt.Println("Starting server on :8080...")
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/ascii", asciiArtHandler)
-	fmt.Println("Starting server on :8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/ascii-art-live", asciiArtLiveHandler)
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -61,4 +62,28 @@ func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
+}
+
+func asciiArtLiveHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	str := r.FormValue("text")
+	bannerStyle := r.FormValue("banner")
+
+	if str == "" || bannerStyle == "" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	art, err := web.PrintAscii(str, bannerStyle)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(art))
 }
