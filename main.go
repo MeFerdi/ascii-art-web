a5a93c7434ec4da540ca46166c5e64b237fc860a
jowala <owalajoab@gmail.com>
2024-07-02 13:43:22 +0300

    chore: add table content

--- main.go
+++ main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
+    "log"
+    "net/http"
 )
 
 func main() {
-	// Define a handler function for handling HTTP requests
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!") // Response to client
-	}
+    // Serve static files (HTML, CSS)
+    fs := http.FileServer(http.Dir("client"))
+    http.Handle("/client/", http.StripPrefix("/client/", fs))
 
-	// Register the handler function for the root route ("/")
-	http.HandleFunc("/", handler)
+    // Define a handler function for handling HTTP requests
+    handler := func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "client/index.html")
+    }
 
-	// Specify the port to listen on
-	port := ":8080" // Note the colon ":" before the port number
+    // Register the handler function for the root route ("/")
+    http.HandleFunc("/", handler)
 
-	// Start the HTTP server
-	fmt.Printf("Server is listening on port %s...\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+    // Specify the port to listen on
+    port := ":8080" // Note the colon ":" before the port number
+
+    // Start the HTTP server
+    fmt.Printf("Server is listening on port %s...\n", port)
+    log.Fatal(http.ListenAndServe(port, nil))
 }
