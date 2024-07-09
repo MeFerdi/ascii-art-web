72e93cad254738c19241246088679b5e00dd381b
jowala <owalajoab@gmail.com>
2024-07-09 14:21:41 +0300

    chore: add comments on the main.go

--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	
 )
 
 func main() {
-	// Serve static files (HTML, CSS)
+	// Create a file server for the "templates" directory
 	fs := http.FileServer(http.Dir("templates"))
+
+	// Handle requests starting with "/templates/" by stripping the prefix 
+	// and serving files from "templates" directory
 	http.Handle("/templates/", http.StripPrefix("/templates/", fs))
 
-	// Define a handler function for handling HTTP requests
+	// Define a handler function for the root route ("/")
 	handlerHome := func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "templates/index.html")
 	}
@@ -19,10 +23,10 @@ func main() {
 	// Register the handler function for the root route ("/")
 	http.HandleFunc("/", handlerHome)
 
+	
 	// Specify the port to listen on
-	port := ":8080" // Note the colon ":" before the port number
+	port := ":8080"
 
-	// str := r.FromValue("text")
 	// Start the HTTP server
 	fmt.Printf("Server is listening on port %s...\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
