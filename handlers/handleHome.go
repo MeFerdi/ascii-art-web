6da30852b4bf14541579105cc1d48be39c4570aa
jowala <owalajoab@gmail.com>
2024-07-10 14:24:11 +0300

    Add files - 400.html - 500.html - 405.html

--- handlers/handleHome.go
+++ handlers/handleHome.go
@@ -1,16 +1,26 @@
 package handlers
 
 import (
+	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
+// Serve the index.html file
+
+var templates = template.Must(template.ParseFiles(filepath.Join("templates", "index.html")))
+
 func HandlerHome(w http.ResponseWriter, r *http.Request) {
-	// Serve the index.html file
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusNotFound)
+		http.ServeFile(w, r, "templates/404.html")
 		return
 	}
 
-	// For GET requests or initial load, serve the index.html file
-	http.ServeFile(w, r, "templates/index.html")
+	err := templates.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		http.ServeFile(w, r, "templates/500.html")
+		return
+	}
 }
