50a13525c28165d117dc50cbb5f861b273f1ff5e
jowala <owalajoab@gmail.com>
2024-07-15 16:39:12 +0300

    chore: add styling

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"net/http"
+
 	"web/handler"
 )
 
 func main() {
+	// Serve Static files (HTML/CSS)
+	fs := http.FileServer(http.Dir("templates"))
+	http.Handle("/templates/", http.StripPrefix("/templates/", fs))
 
-	http.HandleFunc("/",handler.Handler)
-
+	http.HandleFunc("/", handler.Handler)
 
 	port := ":8080"
-	http.ListenAndServe(port,nil)
+	http.ListenAndServe(port, nil)
 }
