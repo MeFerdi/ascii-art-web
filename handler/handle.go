5fa0bb34281883b0f2a6ec81847ac14db218f1c1
jowala <owalajoab@gmail.com>
2024-07-15 16:09:19 +0300

    chore: serve index.html file

--- handler/handle.go
+++ handler/handle.go
@@ -3,5 +3,5 @@ package handler
 import "net/http"
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	
+	http.ServeFile(w, r, "templates/index.html")
 }
--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"net/http"
+	"web/handler"
+)
+
 func main() {
+
+	http.HandleFunc("/",handler.Handler)
+
+
+	port := ":8080"
+	http.ListenAndServe(port,nil)
 }
