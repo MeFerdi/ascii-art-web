16e065b5f908beb74b0693515fa1b23795ccb4b2
ferdyodhiambo <oferdinanddev112@gmail.com>
2024-07-17 14:11:46 +0300

    Fix: removed unnecessary log for printing error messages

--- main.go
+++ main.go
@@ -12,9 +12,29 @@ func main() {
 	fs := http.FileServer(http.Dir("templates"))
 	http.Handle("/templates/", http.StripPrefix("/templates/", fs))
 
-	http.HandleFunc("/", web.Handler)
-	http.HandleFunc("/ascii", web.AsciiArtHandler)
-	http.HandleFunc("/ascii-art-live", web.AsciiArtLiveHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			web.Handler(w, r)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+
+	http.HandleFunc("/ascii", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/ascii" {
+			web.AsciiArtHandler(w, r)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+
+	http.HandleFunc("/ascii-art-live", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/ascii-art-live" {
+			web.AsciiArtLiveHandler(w, r)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
 
 	port := ":8080"
 	fmt.Printf("Starting Server at port %v  \n", port)
